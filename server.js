ae630a5b74e05f6a6fa1854d3adf20fe296bd6d6
Josh Lindsey <josh@core-apps.com>
2013-05-22 00:09:11 -0400

    Add staticCache middleware

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var connect = require('connect');
 
 var port = process.env['PORT'] || 3001;
-var app = connect().use(connect.static('public')).listen(port, function() {
+var app = connect().
+  use(connect.static('public')).
+  use(connect.staticCache());
+  
+app.listen(port, function() {
   console.log("Listening on port "+port);
 });
