4bf7c3cb43a52dd9f83b6a4c87daf3ef003a623c
Josh Lindsey <josh@core-apps.com>
2013-05-23 23:24:46 -0400

    Add caching headers to server

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var connect = require('connect');
 
 var port = process.env['PORT'] || 3001;
+var setHeaders = function(req, res, next) {
+  res.setHeader("Cache-Control", "public, max-age=345600"); // 4 days
+  res.setHeader("Expires", new Date(Date.now() + 345600000).toUTCString());
+  return next();
+};
 var app = connect().
+  use(setHeaders).
   use(connect.static('public')).
   use(connect.staticCache());
   
